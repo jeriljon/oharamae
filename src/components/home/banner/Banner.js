fbeabaf51108b12957fdd27b4a20ffa3dfd9c9f8
Jeril John Panicker <jeriljon@gmail.com>
2024-03-17 22:38:18 -0400

    Intermediate commit

---
+++ src/components/home/banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Banner.module.css'; 
+import bannerImage from '../../../assets/images/banner.jpg'
+
+function Banner() {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.textContent}>
+        <div className={styles.title}>TANGOSEED</div>
+        <div className={styles.caption}>Transform Predict Empower</div>
+      </div>
+      <div className={styles.imageContainer}>
+        <img src={bannerImage} alt="Banner" className={styles.bannerImage} />
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
